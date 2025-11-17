85c1847d4df73fc0ff6a2d788839c3f5973ccd9a
1t0t0 <totophandolack915@gmail.com>
2025-11-17 16:48:54 +0700

    login form

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -2,60 +2,95 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import React, { useContext, useState } from 'react';
-import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
 import { LayoutContext } from '../../../../layout/context/layoutcontext';
 import { InputText } from 'primereact/inputtext';
 import { classNames } from 'primereact/utils';
+import { RiLockPasswordLine } from "react-icons/ri";
+import { LuSquareUser } from "react-icons/lu";
+
+
+
+
 
 const LoginPage = () => {
     const [password, setPassword] = useState('');
-    const [checked, setChecked] = useState(false);
+    // const [checked, setChecked] = useState(false);
     const { layoutConfig } = useContext(LayoutContext);
 
     const router = useRouter();
-    const containerClassName = classNames('surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+    const containerClassName = classNames('login-background flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
 
     return (
         <div className={containerClassName}>
-            <div className="flex flex-column align-items-center justify-content-center">
-                <img src={`/layout/images/logo-${layoutConfig.colorScheme === 'light' ? 'dark' : 'white'}.svg`} alt="Sakai logo" className="mb-5 w-6rem flex-shrink-0" />
-                <div
-                    style={{
-                        borderRadius: '56px',
-                        padding: '0.3rem',
-                        background: 'linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)'
-                    }}
-                >
-                    <div className="w-full surface-card py-8 px-5 sm:px-8" style={{ borderRadius: '53px' }}>
+            <div className="flex flex-column align-items-center justify-content-center w-full px-3">
+                
+                <div className='shadow-5 border-round-2xl bg-blue-800 w-full' style={{ maxWidth: '550px' }}>
+                    <div className="w-full surface-card py-6 px-4 sm:py-8 sm:px-8 border-round-2xl">
                         <div className="text-center mb-5">
-                            <img src="/demo/images/login/avatar.png" alt="Image" height="50" className="mb-3" />
-                            <div className="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
-                            <span className="text-600 font-medium">Sign in to continue</span>
+                            <img src="/layout/images/edl_logo.png" alt="Image" className="mb-3" style={{ height: '80px', maxHeight: '100px' }} />
+                            <div className="text-900 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-medium mb-3">
+                                ລະບົບຄຸ້ມຄອງພາຫະນະ ຟຟລ
+                            </div>
                         </div>
 
                         <div>
-                            <label htmlFor="email1" className="block text-900 text-xl font-medium mb-2">
-                                Email
-                            </label>
-                            <InputText id="email1" type="text" placeholder="Email address" className="w-full md:w-30rem mb-5" style={{ padding: '1rem' }} />
-
-                            <label htmlFor="password1" className="block text-900 font-medium text-xl mb-2">
-                                Password
-                            </label>
-                            <Password inputId="password1" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" toggleMask className="w-full mb-5" inputClassName="w-full p-3 md:w-30rem"></Password>
-
-                            <div className="flex align-items-center justify-content-between mb-5 gap-5">
-                                <div className="flex align-items-center">
-                                    <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
-                                    <label htmlFor="rememberme1">Remember me</label>
+                            <div className='flex align-items-center mb-2'>
+                                <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
+                                    <LuSquareUser size={20} className="mr-2 text-900" />
+                                    <label htmlFor="email" className="text-900 text-lg sm:text-xl font-medium">
+                                        ອີເມວ 
+                                    </label>
                                 </div>
-                                <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
-                                    Forgot password?
-                                </a>
                             </div>
-                            <Button label="Sign In" className="w-full p-3 text-xl" onClick={() => router.push('/')}></Button>
+
+                            <InputText 
+                                id="email" 
+                                type="text" 
+                                placeholder="ກະລຸນາປ້ອນອີເມວຂອງທ່ານ" 
+                                className="w-full mb-4 sm:mb-5" 
+                                style={{ padding: '0.875rem 1rem' }} 
+                            />
+
+                            <div className='flex align-items-center mb-2'>
+                                <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
+                                    <RiLockPasswordLine size={20} className="mr-2 text-900" />
+                                    <label htmlFor="password1" className="text-900 font-medium text-lg sm:text-xl">
+                                        ລະຫັດຜ່ານ
+                                    </label>
+                                </div>
+                            </div>
+                            
+                            <Password 
+                                inputId="password1" 
+                                value={password} 
+                                onChange={(e) => setPassword(e.target.value)} 
+                                placeholder="ກະລຸນາປ້ອນລະຫັດຜ່ານຂອງທ່ານ"  
+                                className="w-full mb-4 sm:mb-5" 
+                                inputClassName="w-full" 
+                                inputStyle={{ padding: '0.875rem 1rem' }}
+                                feedback={false}
+                                toggleMask
+                            />
+
+                            <div className="flex align-items-center justify-content-between mb-4 sm:mb-5 gap-3 sm:gap-5">
+                                {/* <div className="flex align-items-center">
+                                    <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
+                                    <label htmlFor="rememberme1">ຈົດຈຳ</label>
+                                </div> */}
+                                {/* <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
+                                    ລືມລະຫັດຜ່ານ?
+                                </a> */}
+                            </div>
+                            
+                            <Button 
+                                label="ເຂົ້າສູ່ລະບົບ" 
+                                // icon={<MdLogin size={22} className="mr-2" />}
+                                className="login-background w-full text-lg sm:text-xl border-none" 
+                                style={{ padding: '0.875rem 1rem' }}
+                                onClick={() => router.push('/')}
+                            />
                         </div>
                     </div>
                 </div>
