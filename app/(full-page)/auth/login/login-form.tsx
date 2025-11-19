3efcf31bd558c37a3801a266c7a51f204585e226
1t0t0 <totophandolack915@gmail.com>
2025-11-19 09:50:57 +0700

    combine login form to login page

---
+++ app/(full-page)/auth/login/login-form.tsx
@@ -0,0 +1,146 @@
+'use client'
+
+import { LoginInput, LoginSchema } from '@/global/validators/login.schema';
+import { useEffect, useState } from 'react';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+import { authenStore } from '@/app/store/user/loginAuthStore';
+import { useUsersStore } from '@/app/store/user/userStore';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import { LuSquareUser } from 'react-icons/lu';
+
+interface ILoginFormProps {
+}
+
+export default function LoginForm(props: ILoginFormProps) {
+
+  const [isLoading, setLoading] = useState(false)
+  const { setAuthData } = authenStore();
+  const { loginUser } = useUsersStore();
+  const router = useRouter()
+
+  const defaultRowItem = {
+    username: '',
+    password: '',
+  }
+
+  const { control, watch, setValue, register, handleSubmit, formState: { errors }, reset,
+
+  } = useForm<LoginInput>(
+    {
+      resolver: zodResolver(LoginSchema),
+      defaultValues: defaultRowItem,
+    }
+  );
+  useEffect(() => {
+    console.log("errors", errors)
+  }, [errors])
+
+  const onSubmit: SubmitHandler<LoginInput> = async (data) => {
+    const require = {
+      "username": data.username,
+      "password": data.password
+    }
+
+    try {
+      setLoading(true);
+      const resp: any = await loginUser(require);
+      if (resp.status === 201 || resp.status === 200) {
+        toast.success('ຍິນດີຕ້ອນຮັບ')
+        localStorage.setItem('token', resp?.data?.accessToken);
+
+        // Save login timeStamp
+        localStorage.setItem('lastLoginTime', Date.now().toString())
+
+        setAuthData(resp?.data)
+
+        setTimeout(() => {
+          router.push('/')
+          setLoading(false)
+        }, 2000)
+      }
+
+
+
+    } catch (error) {
+      setLoading(false)
+
+    }
+
+  }
+
+  return (
+    <form id='LoginForm' onSubmit={handleSubmit(onSubmit)}>
+      <div className='flex align-items-center mb-2'>
+        <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
+          <LuSquareUser size={20} className="mr-2 text-900" />
+          <label htmlFor="email" className="text-900 text-lg sm:text-xl font-medium">
+            ອີເມວ
+          </label>
+        </div>
+      </div>
+
+      <div className="mb-4 sm:mb-5">
+        <InputText
+          {...register('username')}
+          id="username"
+          type="text"
+          placeholder="ກະລຸນາປ້ອນອີເມວຂອງທ່ານ"
+          className="w-full"
+          style={{ padding: '0.875rem 1rem' }}
+        />
+        {errors?.username?.message ? <span style={{ color: 'red' }}>{errors?.username?.message}</span> : ''}
+      </div>
+
+
+      <div className="mb-4 sm:mb5">
+        <div className='flex align-items-center mb-2'>
+          <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
+            <RiLockPasswordLine size={20} className="mr-2 text-900" />
+            <label htmlFor="password1" className="text-900 font-medium text-lg sm:text-xl">
+              ລະຫັດຜ່ານ
+            </label>
+          </div>
+        </div>
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <Password
+              value={value}
+              onChange={onChange}
+              placeholder="ກະລຸນາປ້ອນລະຫັດຜ່ານຂອງທ່ານ"
+              className="w-full"
+              inputClassName="w-full"
+              inputStyle={{ padding: '0.875rem 1rem' }}
+              feedback={false}
+              toggleMask
+            />
+          )}
+        />
+        {errors?.password?.message ? <span style={{ color: 'red' }}>{errors?.password?.message}</span> : ''}
+      </div>
+
+
+
+      <Button
+        loading={isLoading}
+        loadingIcon={
+          <i className="pi pi-spinner pi-spin" style={{ fontSize: '2rem' }} />
+        }
+        label={isLoading ? '' : 'ເຂົ້າສູ່ລະບົບ'}
+        className="login-background w-full mt-4 text-lg sm:text-xl border-none flex justify-center items-center"
+        style={{
+          padding: '0.875rem 1rem',
+          transition: 'all 0.3s ease-in-out'
+        }}
+      />
+    </form>
+  );
+}
--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -1,28 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-import { useRouter } from 'next/navigation';
 import React, { useContext, useState } from 'react';
-import { Button } from 'primereact/button';
-import { Password } from 'primereact/password';
 import { LayoutContext } from '../../../../layout/context/layoutcontext';
-import { InputText } from 'primereact/inputtext';
 import { classNames } from 'primereact/utils';
-import { RiLockPasswordLine } from "react-icons/ri";
-import { LuSquareUser } from "react-icons/lu";
-import LoginForm from './login';
+import LoginForm from './login-form';
+
 
 const LoginPage = () => {
-    const [password, setPassword] = useState('');
-    // const [checked, setChecked] = useState(false);
     const { layoutConfig } = useContext(LayoutContext);
 
-    const router = useRouter();
     const containerClassName = classNames('login-background flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden', { 'p-input-filled': layoutConfig.inputStyle === 'filled' });
+
+
 
     return (
         <div className={containerClassName}>
             <div className="flex flex-column align-items-center justify-content-center w-full px-3">
-                
+
                 <div className='shadow-5 border-round-2xl bg-blue-800 w-full' style={{ maxWidth: '550px' }}>
                     <div className="w-full surface-card py-6 px-4 sm:py-8 sm:px-8 border-round-2xl">
                         <div className="text-center mb-5">
@@ -31,68 +25,11 @@ const LoginPage = () => {
                                 ລະບົບຄຸ້ມຄອງພາຫະນະ ຟຟລ
                             </div>
                         </div>
-
-                        <div>
-                            <div className='flex align-items-center mb-2'>
-                                <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
-                                    <LuSquareUser size={20} className="mr-2 text-900" />
-                                    <label htmlFor="email" className="text-900 text-lg sm:text-xl font-medium">
-                                        ອີເມວ 
-                                    </label>
-                                </div>
-                            </div>
-
-                            <InputText 
-                                id="email" 
-                                type="text" 
-                                placeholder="ກະລຸນາປ້ອນອີເມວຂອງທ່ານ" 
-                                className="w-full mb-4 sm:mb-5" 
-                                style={{ padding: '0.875rem 1rem' }} 
-                            />
-
-                            <div className='flex align-items-center mb-2'>
-                                <div className='flex align-items-center border-bottom-1 border-primary pb-1' style={{ width: 'fit-content' }}>
-                                    <RiLockPasswordLine size={20} className="mr-2 text-900" />
-                                    <label htmlFor="password1" className="text-900 font-medium text-lg sm:text-xl">
-                                        ລະຫັດຜ່ານ
-                                    </label>
-                                </div>
-                            </div>
-                            
-                            <Password 
-                                inputId="password1" 
-                                value={password} 
-                                onChange={(e) => setPassword(e.target.value)} 
-                                placeholder="ກະລຸນາປ້ອນລະຫັດຜ່ານຂອງທ່ານ"  
-                                className="w-full mb-4 sm:mb-5" 
-                                inputClassName="w-full" 
-                                inputStyle={{ padding: '0.875rem 1rem' }}
-                                feedback={false}
-                                toggleMask
-                            />
-
-                            <div className="flex align-items-center justify-content-between mb-4 sm:mb-5 gap-3 sm:gap-5">
-                                {/* <div className="flex align-items-center">
-                                    <Checkbox inputId="rememberme1" checked={checked} onChange={(e) => setChecked(e.checked ?? false)} className="mr-2"></Checkbox>
-                                    <label htmlFor="rememberme1">ຈົດຈຳ</label>
-                                </div> */}
-                                {/* <a className="font-medium no-underline ml-2 text-right cursor-pointer" style={{ color: 'var(--primary-color)' }}>
-                                    ລືມລະຫັດຜ່ານ?
-                                </a> */}
-                            </div>
-                            
-                            <Button 
-                                label="ເຂົ້າສູ່ລະບົບ" 
-                                // icon={<MdLogin size={22} className="mr-2" />}
-                                className="login-background w-full text-lg sm:text-xl border-none" 
-                                style={{ padding: '0.875rem 1rem' }}
-                                onClick={() => router.push('/')}
-                            />
-                        </div>
+                        <LoginForm />
                     </div>
                 </div>
             </div>
-            <LoginForm />
+
         </div>
     );
 };
